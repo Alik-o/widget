1b194543a2724cc5f3d8d31c8f66c7a6c2ee2329
Oleg Navoev <navoev.oleg@gmail.com>
2024-06-23 23:27:00 +0500

    добавлена функция даты в модуль widget

--- src/widget.py
+++ src/widget.py
@@ -11,3 +11,14 @@ def mask_account_card(account_info: str) -> str:
         mask_card_number = get_mask_card_number(account_info[-16:])
         mask_card = f"{account_info[:-16]} {mask_card_number}"
         return mask_card
+
+
+def get_data(info_date: str) -> str:
+    """Возвращает дату"""
+    date_list = info_date[:10].split("-")
+    correct_date = ".".join(date_list[::-1])
+    return correct_date
+
+
+if __name__ == "__main__":
+    print(get_data("2018-07-11T02:26:18.671407"))
