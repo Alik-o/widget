07f923069f2abdfc128e3aceaa8932ee9fed8b29
Oleg Navoev <navoev.oleg@gmail.com>
2024-07-03 13:25:46 +0500

    добавление тестов для processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,11 @@
-def filter_by_state(transaction_data: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(transaction_data: list[dict], state: str = "EXECUTED") -> list:
     """Возвращает отфильтрованный список словарей по ключу"""
     filter_transaction_data = []
     for transaction in transaction_data:
-        if transaction["state"] == state:
+        if transaction.get("state") == state:
             filter_transaction_data.append(transaction)
+        elif transaction.get("state"):
+            continue
     return filter_transaction_data
 
 
@@ -11,15 +13,3 @@ def sort_by_date(transaction_data: list[dict], ascending: bool = True) -> list[d
     """Возвращает отсортированный список словарей по дате"""
     sort_transaction_data = sorted(transaction_data, key=lambda date: date["date"], reverse=ascending)
     return sort_transaction_data
-
-
-if __name__ == "__main__":
-    list_transaction_data = [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
-    print(filter_by_state(list_transaction_data))
-
-    print(sort_by_date(list_transaction_data))
