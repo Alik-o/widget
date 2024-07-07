2cf59714fa2a59cfd894186024e81cf6cec6573e
Oleg Navoev <navoev.oleg@gmail.com>
2024-07-07 23:56:28 +0500

    добавлены тесты к функции card_number_generator

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -23,6 +23,15 @@ def test_filter_by_currency(transactions):
         "from": "Счет 19708645243227258542",
         "to": "Счет 75651667383060284188",
     }
+    assert next(function_calling) == {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    }
 
 
 def test_filter_by_currency_no_transfer_currency(transactions):
@@ -40,6 +49,20 @@ def test_transaction_descriptions(transactions):
     assert next(function_calling) == "Перевод организации"
     assert next(function_calling) == "Перевод со счета на счет"
 
+
 def test_transaction_descriptions_no_list(no_data):
     with pytest.raises(StopIteration):
         next(transaction_descriptions(no_data))
+
+
+def test_card_number_generator():
+    generator = card_number_generator(1, 3)
+    assert next(generator) == "0000 0000 0000 0001"
+    assert next(generator) == "0000 0000 0000 0002"
+    assert next(generator) == "0000 0000 0000 0003"
+
+
+def test_card_number_generator_limit_numbers():
+    with pytest.raises(ValueError):
+        next(card_number_generator(10_000_000_000_000_000, 10_000_000_000_000_002))
+
