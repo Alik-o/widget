1ba439e3d5947afb046d89422856dab681eb3383
Oleg Navoev <navoev.oleg@gmail.com>
2024-07-10 23:34:32 +0500

    Добавил тестов к декоратору

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -20,6 +20,19 @@ def test_decorator_log(capsys):
     assert captured.out == "add_numbers ok\n\n"
 
 
+def test_decorator_log_txt():
+    @log("test_log.txt")
+    def add_numbers(a, b):
+        return a + b
+
+    add_numbers(1, 1)
+    with open("test_log.txt") as file:
+        result = None
+        for i in file:
+            result = i
+    assert result == "add_numbers ok\n"
+
+
 def test_log_error(capsys):
     @log()
     def division_numbers(a, b):
@@ -28,3 +41,16 @@ def test_log_error(capsys):
     division_numbers(5, 0)
     captured = capsys.readouterr()
     assert captured.out == "division_numbers error: division by zero. Inputs: (5, 0) {}\n\n"
+
+
+def test_log_error_txt():
+    @log("test_log.txt")
+    def division_numbers(a, b):
+        return a / b
+
+    division_numbers(5, 0)
+    with open("test_log.txt") as file:
+        result = None
+        for i in file:
+            result = i
+    assert result == "division_numbers error: division by zero. Inputs: (5, 0) {}\n"
