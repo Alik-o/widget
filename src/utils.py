0212a6fa93fd12759ae908f90e901d8be3b24c49
Oleg Navoev <navoev.oleg@gmail.com>
2024-07-22 00:19:04 +0500

    Добавлены функция возвращающая сумму транзакции в рублях

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,34 @@
 import json
-import os
+from typing import Any
 
-def financial_transaction_data(path_to: str) -> list:
+from src.external_api import get_currency_conversion
+
+
+def get_financial_transaction_data(path_to: str) -> Any:
     """Возвращает список словарей с данными о финансовых транзакциях"""
     try:
-        with open(path_to, encoding='utf-8') as f:
+        with open(path_to, encoding="utf-8") as f:
             data = json.load(f)
         return data
     except Exception:
         return list()
 
 
-if __name__ == '__main__':
-    print(financial_transaction_data(r".Д./data/operations.json"))
+def get_amount_transaction_rubles(transactions: dict) -> float:
+    """Возвращает сумму транзакции в рублях, если транзакция была не в рублях то конвертирует в рубли"""
+    currency = transactions["operationAmount"]["currency"]["code"]
+    amount = transactions["operationAmount"]["amount"]
+    if currency != "RUB":
+        date = transactions["date"][:10]
+        amount = get_currency_conversion(currency, amount, date)
+    return amount
+
+
+# if __name__ == "__main__":
+#     print(get_financial_transaction_data(r"../data/operations.json"))
+
+
+if __name__ == "__main__":
+    transactions = get_financial_transaction_data(r"../data/operations.json")
+    print(transactions[2])
+    print(get_amount_transaction_rubles(transactions[2]))
