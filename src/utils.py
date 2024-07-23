d3ce4315c81a08f366e273986e13fab75aad5775
Oleg Navoev <navoev.oleg@gmail.com>
2024-07-23 23:29:56 +0500

    Изменение пути в модуле utils для логов

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ logger_transaction_data = logging.getLogger("transaction_data")
 logger_amount_transaction = logging.getLogger("amount_transaction")
 logger_transaction_data.setLevel(logging.DEBUG)
 logger_amount_transaction.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("../logs/utils.log", "w", encoding="utf-8")
+file_handler = logging.FileHandler("./logs/utils.log", "w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger_amount_transaction.addHandler(file_handler)
@@ -41,8 +41,3 @@ def get_amount_transaction_rubles(transactions: dict) -> float:
         amount = get_currency_conversion(currency, amount, date)
     logger_amount_transaction.info("Получена сумма в рублях")
     return amount
-
-
-if __name__ == "__main__":
-    transaction = get_financial_transaction_data(r"..\data\operations.json")
-    print(get_amount_transaction_rubles(transaction[1]))
