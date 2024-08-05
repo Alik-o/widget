4784e63a8fe88044ba8fec24341dd2927c59c9f9
Oleg Navoev <navoev.oleg@gmail.com>
2024-08-06 04:07:23 +0500

    Исправления в utils

--- src/utils.py
+++ src/utils.py
@@ -30,11 +30,13 @@ def get_financial_transaction_data(path_to: str) -> Any:
         elif path_to.endswith(".xlsx"):
             data_xlsx = pd.read_excel(path_to)
             data_xlsx = data_xlsx.loc[data_xlsx.description.notnull()]
+            data_xlsx = data_xlsx.fillna(0)
             data = data_xlsx.to_dict(orient="records")
             logger_transaction_data.info("xlsx файл распакован удачно")
         elif path_to.endswith(".csv"):
             data_csv = pd.read_csv(path_to, delimiter=";")
             data_csv = data_csv.loc[data_csv.description.notnull()]
+            data_csv = data_csv.fillna(0)
             data = data_csv.to_dict(orient="records")
             logger_transaction_data.info("csv файл распакован удачно")
         else:
@@ -49,8 +51,12 @@ def get_financial_transaction_data(path_to: str) -> Any:
 def get_amount_transaction_rubles(transactions: dict) -> float:
     """Возвращает сумму транзакции в рублях, если транзакция была не в рублях то конвертирует в рубли"""
     logger_amount_transaction.info("Получены данные о транзакции")
-    currency = transactions["operationAmount"]["currency"]["code"]
-    amount = transactions["operationAmount"]["amount"]
+    if transactions.get("operationAmount"):
+        currency = transactions["operationAmount"]["currency"]["code"]
+        amount = transactions["operationAmount"]["amount"]
+    else:
+        currency = transactions["currency_code"]
+        amount = transactions["amount"]
     logger_amount_transaction.info("Проверка валюты транзакции")
     try:
         if currency != "RUB":
