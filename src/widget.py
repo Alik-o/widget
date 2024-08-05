5398e4326f9b6ae786125397c826557d03a6f85b
Oleg Navoev <navoev.oleg@gmail.com>
2024-08-06 04:07:03 +0500

    Исправления в widget

--- src/widget.py
+++ src/widget.py
@@ -3,18 +3,11 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(account_info: str) -> str:
     """Возвращает маску карты или счета"""
-    varieties_card = [
-        "Maestro",
-        "MasterCard",
-        "Visa Classic",
-        "Visa Platinum",
-        "Visa Gold",
-    ]
     if account_info[:4] == "Счет" and account_info[-20:].isdigit():
         account_number = get_mask_account(account_info[5:])
         mask_account = f"{account_info[:4]} {account_number}"
         return mask_account
-    elif account_info[:-17] in varieties_card and account_info[-16:].isdigit():
+    elif account_info[-16:].isdigit():
         mask_card_number = get_mask_card_number(account_info[-16:])
         mask_card = f"{account_info[:-16]}{mask_card_number}"
         return mask_card
